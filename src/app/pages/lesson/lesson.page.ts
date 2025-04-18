0458220857ba8dfbd20d75a04b96091574f25289
Shayanny <shayanny4@outlook.com>
2025-04-18 22:00:08 +0100

    Lesson page started

--- src/app/pages/lesson/lesson.page.ts
+++ src/app/pages/lesson/lesson.page.ts
@@ -1,14 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { IonicModule} from '@ionic/angular';
 
 @Component({
   selector: 'app-lesson',
   templateUrl: './lesson.page.html',
   styleUrls: ['./lesson.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonicModule, CommonModule, FormsModule]
 })
 export class LessonPage implements OnInit {
 
